a9c7687c52e2d031c1efa3d50279299c133739ea
Sana Garah <E-s.garah@lean.sa>
2023-02-16 17:55:35 +0300

    removing extra code

--- src/components/pages/pdf-page/index.tsx
+++ src/components/pages/pdf-page/index.tsx
@@ -3,7 +3,7 @@ import PDFCreator from "../../common/pdf-creator";
 
 export default function PDFPage({}) {
   const location = useLocation();
-  console.log(location.state);
+
   return (
     <PDFCreator
       idNumber={location.state.idNumber}
